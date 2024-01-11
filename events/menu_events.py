ee1dc445c5f29e5d402c1b0aa5300c611533ba01
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2024-01-11 10:49:21 +0200

    Add shop menu ui

--- events/menu_events.py
+++ events/menu_events.py
@@ -5,6 +5,7 @@ import sys
 import pygame
 
 ###### My Packages ######
+from pyengine.libs.designer.designer import Designer
 from pyengine.libs.transition.transition import Transition
 
 #### Type Hinting ####
@@ -22,3 +23,8 @@ def background_repeat(background: PyRect):
     if Transition.elements.get(background.group).get(background.name).start_y != -768:
         Transition.elements.get(background.group).get(background.name).start_y = -768
         Transition.elements.get(background.group).get(background.name).duration *= 2
+
+
+def toggle_shop(_):
+    """toggle shop menu"""
+    Designer.toggle_exclude("shop_menu")
